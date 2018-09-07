543bcfee5862b297d750c47cbfbc2d753d6fe5d5
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-09-07 11:18:17 +0700

    int

---
+++ src/Entity/User.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: boonkuaeboonsutta
+ * Date: 7/9/2018 AD
+ * Time: 11:07
+ */
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User extends BaseUser
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+}
